57c72aae437937a0ed3217969c49ffaab15f4104
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-15 14:57:25 -0800

    adding files from previous class

--- src/disney/sedid/chef/util/PropertiesFile.java
+++ src/disney/sedid/chef/util/PropertiesFile.java
@@ -1,22 +1,87 @@
 package disney.sedid.chef.util;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class PropertiesFile {
 
-    //TODO: change to URL and do http GET
     private String pathToFile;
+    private Scanner input;
+    private File propertiesFile;
 
-    //construct PropertiesFile object
-    public PropertiesFile(String pathToFile){
+    public PropertiesFile(String somePath){
 
-        this.pathToFile = pathToFile;
+        this.pathToFile = somePath;
 
-        //Construct file system object
-        //File f = new File(String pathToFile);
+        try
+        {
+            this.propertiesFile = new File(this.pathToFile);
+            this.input = new Scanner(this.propertiesFile);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Caught FileNotFoundException: " + e.getMessage());
+        }
     }
-    //
+
+    public void parsePropertiesFile(){
 
 
+    }
+
+    public void createTemplate ()
+    {
+
+        //test reading file and writing every line to screen
+        while(input.hasNextLine())
+        {
+
+            System.out.println(input.nextLine());
+        }
 
 
+    }
+
+    public void createAttributesFile(File output)
+    {
+
+        try
+        {
+
+        //initialize PrintStream object for write
+        PrintStream attributesFile = new PrintStream(output);
+
+            //test reading file and writing every line to screen
+            while(input.hasNextLine())
+            {
+                //read each line into a String
+                String oneLineString = input.nextLine();
+
+                //instantiate new Scanner object with a single line as input
+                Scanner oneLineScanner = new Scanner(oneLineString);
+
+
+                //output section
+                attributesFile.println(input.nextLine());
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Caught FileNotFoundException: " + e.getMessage());
+        }
+    }
+
+    public String getPathToFile() {
+        return pathToFile;
+    }
+
+    public void setPathToFile(String pathToFile) {
+        this.pathToFile = pathToFile;
+    }
+
+    public String toString(){
+
+        return "Path to file: " + getPathToFile();
+    }
 }
