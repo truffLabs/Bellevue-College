2ddc7a3a708c1a57a8dcdac7e039059413736d62
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-19 12:44:20 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/dork/model/PersonTest.java
@@ -0,0 +1,61 @@
+package edu.bellevuecollege.cs211.dork.model;
+import org.junit.Test;
+
+public class PersonTest {
+    @Test
+    public void testPerson() throws Exception
+    {
+        Person someone = new Person("fun");
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testMove2() throws Exception {
+
+    }
+
+    @Test
+    public void testAddItems() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveItems() throws Exception {
+
+    }
+
+    @Test
+    public void testClearItems() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPersonsName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPersonsName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentRoom() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCurrentRoom() throws Exception {
+
+    }
+
+    @Test
+    public void testGetitemsOnPerson() throws Exception {
+
+    }
+}
