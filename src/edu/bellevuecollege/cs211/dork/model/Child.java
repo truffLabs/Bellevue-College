364e5d98b3f966a719465a5995a325402095bea9
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-19 15:35:06 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/dork/model/Child.java
@@ -0,0 +1,24 @@
+package edu.bellevuecollege.cs211.dork.model;
+
+import edu.bellevuecollege.cs211.dork.items.Item;
+import edu.bellevuecollege.cs211.dork.items.Weapon;
+
+public class Child extends Person
+{
+    public Child(String someName)
+    {
+        super(someName);
+    }
+
+    public String crawl()
+    {
+        return "This is just the beginning of trouble.";
+    }
+
+    public String cry()
+    {
+       return "WAAAAAAAAAAAAAAHHH!!!!!";
+
+    }
+
+}
