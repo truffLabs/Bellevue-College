a6d226936f798f85e69389af7c52e18ae5097358
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-19 15:25:51 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/dork/items/Bat.java
@@ -0,0 +1,33 @@
+package edu.bellevuecollege.cs211.dork.items;
+import edu.bellevuecollege.cs211.dork.model.Person;
+
+public class Bat extends Item implements Weapon{
+
+    public Bat(String itemName)
+    {
+        super(itemName);
+    }
+
+    public String pickUp(){
+
+       return "Don't swing this around in the house.";
+   }
+
+
+   public String close(){
+
+       return "Keep these things moist.";
+   }
+
+   @Override
+   public String attack(Person p)
+   {
+       return "Good thing its only foam.";
+   }
+
+   @Override
+   public String defend(Weapon w)
+   {
+       return "Great choice for defense.";
+   }
+}
