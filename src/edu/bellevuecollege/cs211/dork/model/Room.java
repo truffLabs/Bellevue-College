9979ade509de2905ebbcaa38baa91ff380544acb
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-20 23:55:08 -0800

    added methods for openable and weapon items

--- src/edu/bellevuecollege/cs211/dork/model/Room.java
+++ src/edu/bellevuecollege/cs211/dork/model/Room.java
@@ -1,5 +1,8 @@
 package edu.bellevuecollege.cs211.dork.model;
 import edu.bellevuecollege.cs211.dork.items.Item;
+import edu.bellevuecollege.cs211.dork.items.Openable;
+import edu.bellevuecollege.cs211.dork.items.Weapon;
+
 import java.util.ArrayList;
 
 /**
@@ -20,8 +23,14 @@ public class Room
 	protected Room east;
 	protected Room west;
 
-    //hold items in Room in array list
+    //hold all items in Room in array list
     private ArrayList<Item> itemsInRoom;
+
+    //hold openable items in separate array list
+    private ArrayList<Openable> openableItems;
+
+    //hold weapon items in separate array list
+    private ArrayList<Weapon> weaponItems;
 
     //hold People objects in Room using array list
     private ArrayList<Person> peopleInRoom;
@@ -86,16 +95,16 @@ public class Room
      * initially setting adjacency at beginning of game.
      *
      * @param north Room object north of instance
-     * @param south Room object south of instance
      * @param east Room object east of instance
+     * @param south Room object south of instance
      * @param west Room object west of instance
      */
-    public void setLocation(Room north, Room south, Room east, Room west)
+    public void setLocation(Room north, Room east, Room south, Room west)
     {
 
         this.north = north;
-        this.south = south;
         this.east = east;
+        this.south = south;
         this.west = west;
     }
 
@@ -105,6 +114,7 @@ public class Room
      * @param dir direction from calling instance
      * @return room object Direction dir from calling instance
      */
+    //TODO: redundant to getNorth?
     public Room getAdjacent(Direction dir)
     {
 
@@ -138,6 +148,28 @@ public class Room
     }
 
     /**
+     * Method returns an ArrayList containing Openable objects contained
+     * in calling instance.
+     *
+     * @return ArrayList object containing Openable objects
+     */
+    public ArrayList getOpenableInRoom()
+    {
+        return this.openableItems;
+    }
+
+    /**
+     * Method returns an ArrayList containing Weapon objects contained
+     * in calling instance.
+     *
+     * @return ArrayList object containing Weapon objects
+     */
+    public ArrayList getWeaponInRoom()
+    {
+        return this.weaponItems;
+    }
+
+    /**
      * Method returns an ArrayList containing Person objects contained
      * in calling instance.
      *
@@ -159,6 +191,26 @@ public class Room
     }
 
     /**
+     * Add an Item to the ArrayList of Openable objects in calling Room instance.
+     *
+     * @param o represents Openable object
+     */
+    public void addOpenableItem(Openable o)
+    {
+        openableItems.add(o);
+    }
+
+    /**
+     * Add an Item to the ArrayList of Weapon objects in calling Room instance.
+     *
+     * @param w represents Weapon object
+     */
+    public void addWeaponItem(Weapon w)
+    {
+        weaponItems.add(w);
+    }
+
+    /**
      * Add a Person to the ArrayList of Person objects in calling Room instance.
      *
      * @param p represents Person object
@@ -177,6 +229,28 @@ public class Room
     {
 
         itemsInRoom.remove(i);
+    }
+
+    /**
+     * Removes an openable item from the ArrayList of Openable objects.
+     *
+     * @param o represents an Openable object
+     */
+    public void removeOpenableItem(Openable o)
+    {
+
+        openableItems.remove(o);
+    }
+
+    /**
+     * Removes an weapon item from the ArrayList of Weapon objects.
+     *
+     * @param w represents a Weapon object
+     */
+    public void removeWeaponItem(Weapon w)
+    {
+
+        weaponItems.remove(w);
     }
 
     /**
