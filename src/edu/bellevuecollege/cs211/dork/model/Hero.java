6f18c4791799f164908acfd0a7d7abd590225471
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-21 23:14:09 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/dork/model/Hero.java
@@ -0,0 +1,19 @@
+package edu.bellevuecollege.cs211.dork.model;
+
+public class Hero extends Person
+{
+    public Hero(String someName)
+    {
+        super(someName);
+    }
+
+    public String cheer()
+    {
+        return "Hip, hip, HOORAY!";
+    }
+
+    public String die()
+    {
+        return "I'm meeelllltttiiingggg!";
+    }
+}
