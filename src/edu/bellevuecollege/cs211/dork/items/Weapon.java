93bdd3ddcaa39d886266d80fb02c9784ec80db73
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-22 19:24:31 -0800

    Added javadoc comments and notes

--- src/edu/bellevuecollege/cs211/dork/items/Weapon.java
+++ src/edu/bellevuecollege/cs211/dork/items/Weapon.java
@@ -1,9 +1,14 @@
 package edu.bellevuecollege.cs211.dork.items;
-
 import edu.bellevuecollege.cs211.dork.model.Person;
 
+/**
+ *Interface to be extended by Items. Should represent "things" that can be used as weapons.
+ */
 public interface Weapon 
 {
+    //what happens when this object attacks a person?
 	public String attack (Person p);
+
+    //what happens when this object is used to defend against a weapon?
 	public String defend (Weapon w);
 }
