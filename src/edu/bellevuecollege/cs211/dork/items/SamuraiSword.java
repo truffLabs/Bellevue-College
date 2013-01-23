8749af4dcca45f4706a37a3b3769e7bb53ff02cc
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-22 19:11:17 -0800

    Added javadoc comments and putInSheath method.

--- src/edu/bellevuecollege/cs211/dork/items/SamuraiSword.java
+++ src/edu/bellevuecollege/cs211/dork/items/SamuraiSword.java
@@ -1,6 +1,12 @@
 package edu.bellevuecollege.cs211.dork.items;
 import edu.bellevuecollege.cs211.dork.model.*;
 
+/**
+ *Class defines a SamuraiSword object. This class extends Item and
+ * implements the Weapon interface. Intended to represent a samurai sword
+ * (the kind that samurai's use).
+ */
+
 public class SamuraiSword extends Item implements Weapon{
 
     public SamuraiSword(String itemName)
@@ -21,5 +27,10 @@ public class SamuraiSword extends Item implements Weapon{
    public String defend(Weapon w)
    {
        return "You really need to move faster.";
+   }
+
+   public String putInSheath()
+   {
+       return "Way to keep this thing safe.";
    }
 }
