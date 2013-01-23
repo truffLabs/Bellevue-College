0a1dcb3521600aa4df8d500aabbfb020e62befa5
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-22 19:38:34 -0800

    added javadoc comments

--- src/edu/bellevuecollege/cs211/dork/model/Zombie.java
+++ src/edu/bellevuecollege/cs211/dork/model/Zombie.java
@@ -1,5 +1,10 @@
 package edu.bellevuecollege.cs211.dork.model;
 
+/**
+ *Class defines a Zombie object. This class extends Person.
+ * The intent is that Zombie objects are the "bad guys" in the game.
+ * Only Zombie objects can "die".
+ */
 public class Zombie extends Person
 {
     public Zombie(String someName)
