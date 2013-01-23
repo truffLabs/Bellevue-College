439c0d9e292e0f69133ab8e75de614539f59ca69
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-22 19:22:48 -0800

    Added javadoc comments and notes

--- src/edu/bellevuecollege/cs211/dork/items/Openable.java
+++ src/edu/bellevuecollege/cs211/dork/items/Openable.java
@@ -1,7 +1,13 @@
 package edu.bellevuecollege.cs211.dork.items;
 
+/**
+ *Interface to be extended by Items. Should represent "things" that can be opened.
+ */
 public interface Openable
 {
+    //what happens when you open the item?
 	public String open();
+
+    //what happens when you close the item?
 	public String close();
 }
