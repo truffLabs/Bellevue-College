8a7b8b853c0f55c52cb7ce55f3c6eceb06e50cd2
Pat Kaehuaea <uncleakamai@gmail.com>
2013-01-25 23:05:51 -0800

    complete exercise 9

--- src/edu/bellevuecollege/cs211/chapter10/Homework2.java
+++ src/edu/bellevuecollege/cs211/chapter10/Homework2.java
@@ -1,6 +1,7 @@
 package edu.bellevuecollege.cs211.chapter10;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Homework2
 {
@@ -23,15 +24,111 @@ public class Homework2
         System.out.println(hamlet);
         **/
 
+        /**
         //exercise5: [4, 1, 2, 0, 3]
         //TODO: refresh why ArrayList can't contain primitive types
         ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(4);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(0);
+        numbers.add(3);
+
+        System.out.println("Numbers before: " + numbers);
+
+        scaleByK(numbers);
+
+        System.out.println("Numbers after: " + numbers);
+        **/
+
+        //exercise 9:
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(7);
+        numbers.add(2);
+        numbers.add(0);
+        numbers.add(0);
+        numbers.add(4);
+        numbers.add(0);
+        numbers.add(9);
+        numbers.add(0);
+        numbers.add(6);
+        numbers.add(4);
+        numbers.add(8);
+
+        System.out.println("List of numbers: " + numbers);
+        System.out.println("Number of idexes apart the two furthest occurrences of the number 0 are: "
+                + rangeBetweenZeroes(numbers));
 
     }
 
-    public static void scaleByK(ArrayList someList)
+    public static int rangeBetweenZeroes(ArrayList<Integer> someList)
     {
+        int numZeroes = 0;
+        int firstZero = 0;
+        int lastZero = 0;
 
+        //iterate through elements and count the occurrences of '0'
+        for(int i = 0 ; i < someList.size() ; i++)
+        {
+            //count the number of zeroes in the range
+            if(someList.get(i).intValue() == 0 && numZeroes == 0)
+            {
+                numZeroes++;
+
+                //set index for first occurrence of zero
+                firstZero = i;
+            }
+            else if(someList.get(i).intValue() == 0 && numZeroes > 0)
+            {
+                numZeroes++;
+
+                //every time a new zero is found, set index to lastZero
+                lastZero = i;
+            }
+        }
+
+        //do something when you know how many zeroes are in the range
+        if(numZeroes == 0)
+        {
+            return 0;
+        }
+        else if(numZeroes == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return (lastZero - firstZero) +1;
+        }
+    }
+
+    public static void scaleByK(ArrayList<Integer> someList)
+    {
+        int count = 0;
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+
+        //read through Integer elements of someList
+        for(Integer i : someList)
+        {
+            //ignore '0' and negative elements
+            if(i.intValue() > 0)
+            {
+                count = i.intValue();
+
+                //write integer value into second ArrayList 'count' times
+                for(int j =0 ; j < count ; j++)
+                {
+                    tempList.add(count);
+                }
+
+            }
+        }
+
+        //clear the initial list
+        someList.clear();
+
+        //add the elements from tempList back to someList
+        someList.addAll(0, tempList);
     }
 
     public static void swapPairs(ArrayList someList)
