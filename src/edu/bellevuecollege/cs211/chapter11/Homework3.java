a9a81a83a3bc032b7683f414283e0d0141b36739
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-02 16:03:25 -0800

    completed 16 with tests ; still get nullpointer exception if null is value for map

--- src/edu/bellevuecollege/cs211/chapter11/Homework3.java
+++ src/edu/bellevuecollege/cs211/chapter11/Homework3.java
@@ -1,9 +1,9 @@
 package edu.bellevuecollege.cs211.chapter11;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import com.sun.servicetag.SystemEnvironment;
+
+import java.util.*;
+
 
 /**
  * Created with IntelliJ IDEA. User: pakaehua Date: 1/30/13 Time: 5:21 PM To change this template use File | Settings |
@@ -13,6 +13,7 @@ public class Homework3
 {
     public static void main(String Args[])
     {
+        /**
         //exercise 5: sortAndRemoveDuplicates
         List<Integer> someList = new ArrayList<Integer>();
 
@@ -24,10 +25,100 @@ public class Homework3
 
         System.out.println(someList);
 
+        //exercise 9: hasOdd
+        Set<Integer> someSet = new HashSet<Integer>();
+        someSet.add(1) ; someSet.add(-4) ; someSet.add(6) ; someSet.add(8);
+        System.out.println(hasOdd(someSet));
+
+        //exercise 15: maxOccurrences
+        List<Integer> someList2 = new ArrayList<Integer>();
+        someList2.add(1); someList2.add(1); someList2.add(3); someList2.add(5); someList2.add(7);
+        System.out.println(maxOccurrences(someList2));
+
+        //exercise 16: is1to1
+        Map<String, String> m1 = new HashMap<String, String>();
+        m1.put("Marty", "206-9024"); m1.put("Hawking", "123-4567"); m1.put("Smith", "949-0504");
+        m1.put("Newton", "123-4567");
+        System.out.println(is1to1(m1));
+        **/
+
 
     }
 
+    //exercise16
+    public static Boolean is1to1(Map<String, String> m)
+    {
+        String value = null;
 
+        for(Map.Entry<String, String> entry : m.entrySet())
+        {
+            if(entry.getValue().equals(value))
+            {
+                return false;
+            }
+
+            value = entry.getValue();
+        }
+
+        return true;
+    }
+
+    //exercise 15
+    public static int maxOccurrences(List<Integer> l)
+    {
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+
+        //set placeholder for key at maximum value
+        int keyAtMaxValue = 0;
+        //set max number of times a value is found
+        int maxOccurrence = 0;
+
+        //add elements in list to map with occurrences
+        for(Integer i : l)
+        {
+            //if the integer already exists as a key ; increment value
+            if(m.containsKey(i))
+            {
+                int tempCount = m.get(i) + 1;
+                m.put(i, tempCount);
+            }
+            //else add integer as key
+            else
+            {
+                m.put(i, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : m.entrySet())
+        {
+
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if(value >= maxOccurrence)
+            {
+                maxOccurrence = value;
+            }
+        }
+
+        return maxOccurrence;
+    }
+
+    //exercise 9
+    public static Boolean hasOdd(Set<Integer> s)
+    {
+        //return true if set has at least one odd integer ; false otherwise ; return false if set is empty
+
+        for(Integer i : s)
+        {
+            if(i % 2 != 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     //exercise 5
     public static void sortAndRemoveDuplicates(List<Integer> l)
@@ -41,10 +132,6 @@ public class Homework3
         //clear list and add all elements of set into list
         l.clear();
         l.addAll(s);
-
-
-
-
     }
 
 }
--- src/edu/bellevuecollege/cs211/chapter11/Homework3Test.java
+++ src/edu/bellevuecollege/cs211/chapter11/Homework3Test.java
@@ -1,15 +1,116 @@
+
 package edu.bellevuecollege.cs211.chapter11;
 import org.junit.Assert;
 import org.junit.Test;
 import static edu.bellevuecollege.cs211.chapter11.Homework3.*;
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Homework3Test
 {
+
+    //exercise 16
+    @Test
+    public void testIs1to1() throws Exception
+    {
+
+        Map<String, String> m1 = new HashMap<String, String>();
+        m1.put("Marty", "206-9024"); m1.put("Hawking", "123-4567"); m1.put("Smith", "949-0504");
+        m1.put("Newton", "123-4567");
+        assertFalse(Homework3.is1to1(m1));
+
+        Map<String, String> m2 = new HashMap<String, String>();
+        m2.put("Marty", "206-9024"); m2.put("Hawking", "555-1234"); m2.put("Smith", "949-0504");
+        m2.put("Newton", "123-4567");
+        assertTrue(Homework3.is1to1(m2));
+
+        Map<String, String> m3 = new HashMap<String, String>();
+        m3.put("1", ""); m3.put("2", "");
+        assertFalse(Homework3.is1to1(m3));
+
+        Map<String, String> m4 = new HashMap<String, String>();
+        m4.put("1", "abc"); m4.put("2", "");
+        assertTrue(Homework3.is1to1(m4));
+
+        Map<String, String> m5 = new HashMap<String, String>();
+        m5.put("1", ""); m5.put("2", "abc");
+        assertTrue(Homework3.is1to1(m5));
+
+        Map<String, String> m6 = new HashMap<String, String>();
+        m6.put("1", ""); m6.put("2", ""); m6.put("3", "");m6.put("4", "");
+        assertFalse(Homework3.is1to1(m6));
+
+
+        Map<String, String> m7 = new HashMap<String, String>();
+        m7.put("1", "");
+        assertTrue(Homework3.is1to1(m7));
+
+
+        Map<String, String> m8 = new HashMap<String, String>();
+        m8.put(null, "123");
+        assertTrue(Homework3.is1to1(m8));
+
+        Map<String, String> m9 = new HashMap<String, String>();
+        m9.put(null, "123"); m9.put(null, "123"); m9.put(null, "123");
+        assertTrue(Homework3.is1to1(m9));
+
+        //Map<String, String> m10 = new HashMap<String, String>();
+        //m10.put("1", "123"); m10.put("2", null); m10.put(null, "123");
+        //assertTrue(Homework3.is1to1(m10));
+
+
+    }
+
+    //exercise 15
+    @Test
+    public void testMaxOccurrences() throws Exception
+    {
+        //no reoccurring number
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(1); l.add(2); l.add(3); l.add(5); l.add(7);
+        assertEquals(Homework3.maxOccurrences(l), 1);
+
+        //one reoccurring number
+        List<Integer> l2 = new ArrayList<Integer>();
+        l2.add(1); l2.add(1); l2.add(3); l2.add(5); l2.add(7);
+        assertEquals(Homework3.maxOccurrences(l2), 2);
+
+        //empty list
+        List<Integer> l3 = new ArrayList<Integer>();
+        assertEquals(Homework3.maxOccurrences(l3), 0);
+
+        //two reoccurring numbers
+        List<Integer> l4 = new ArrayList<Integer>();
+        l4.add(1); l4.add(1); l4.add(1); l4.add(2); l4.add(2);
+        assertEquals(Homework3.maxOccurrences(l4), 3);
+
+
+
+    }
+
+    //exercise 9
+    @Test
+    public void testHasOdd() throws Exception
+    {
+        Set<Integer> someSet = new HashSet<Integer>();
+        someSet.add(1) ; someSet.add(-4) ; someSet.add(6) ; someSet.add(8);
+        assertTrue(Homework3.hasOdd(someSet));
+
+        Set<Integer> someSet2 = new HashSet<Integer>();
+        someSet2.add(-2) ; someSet2.add(0) ; someSet2.add(6) ; someSet2.add(8);
+        assertFalse(Homework3.hasOdd(someSet2));
+
+        Set<Integer> someSet3 = new HashSet<Integer>();
+        someSet3.add(-1) ; someSet3.add(-3) ; someSet3.add(-5) ; someSet3.add(-7);
+        assertTrue(Homework3.hasOdd(someSet3));
+
+        Set<Integer> someSet4 = new HashSet<Integer>();
+        assertFalse(Homework3.hasOdd(someSet4));
+    }
+
+
+    //exercise 5
     @Test
     public void testSortAndRemoveDuplicates() throws Exception
     {
@@ -20,7 +121,6 @@ public class Homework3Test
         sortAndRemoveDuplicates(someList);
         Assert.assertEquals(someList.toString(), "[-9, -5, 4, 7, 8, 11, 15, 27, 32]");
 
-
         List<Integer> someList2 = new ArrayList<Integer>();
         someList2.add(11); someList2.add(11); someList2.add(11); someList2.add(11); someList2.add(12);
         sortAndRemoveDuplicates(someList2);
@@ -30,4 +130,5 @@ public class Homework3Test
         sortAndRemoveDuplicates(someList3);
         Assert.assertEquals(someList3.toString(), "[]");
     }
+
 }
