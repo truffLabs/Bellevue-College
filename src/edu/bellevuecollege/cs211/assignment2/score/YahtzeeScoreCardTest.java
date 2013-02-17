f009a47ca187141782a561ec4c35508f6f0001f3
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-17 15:28:35 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/assignment2/score/YahtzeeScoreCardTest.java
@@ -0,0 +1,89 @@
+package edu.bellevuecollege.cs211.assignment2.score;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA. User: pakaehua Date: 2/17/13 Time: 3:28 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class YahtzeeScoreCardTest
+{
+    //TODO: Bring most of test logic for conditions into this rather than in the Yahtzee test class
+    @Test
+    public void testAddToUpperScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testAddToLowerScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testCalcCurrentScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testCalcTotalScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetAvailableScoreTypes() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetUpperScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testSetUpperScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetLowerScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testSetLowerScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetCurrentScore() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetCountYahtzees() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testSetCountYahtzees() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetFinalScore() throws Exception
+    {
+
+    }
+}
