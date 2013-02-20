5efa010f0653a25984de64ac4d4264cebe7dfae4
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-19 22:50:11 -0800

    added tests for three complete game walkthroughs

--- src/edu/bellevuecollege/cs211/assignment2/score/YahtzeeScoreCardTest.java
+++ src/edu/bellevuecollege/cs211/assignment2/score/YahtzeeScoreCardTest.java
@@ -12,6 +12,171 @@ import static org.junit.Assert.assertTrue;
 public class YahtzeeScoreCardTest
 {
     @Test
+    public void testRunScoringScenario() throws Exception
+    {
+        //*** Game 1: 0 Bonus, 1 Yahtzee ***
+        Yahtzee y1 = new Yahtzee();
+
+        //Turn 1
+        y1.getDiceCollection().setValues(1, 1, 3, 3, 3);
+        assertEquals(2, y1.scoreOnes());
+        //Turn 2
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(6, y1.scoreTwos());
+        //Turn 3
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(3, y1.scoreThrees());
+        //Turn 4
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(0, y1.scoreFours());
+        //Turn 5
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 5);
+        assertEquals(5, y1.scoreFives());
+        //Turn 6
+        y1.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(30, y1.scoreSixes());
+        //3 of a kind
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(10, y1.scoreThreeOfAKind());
+        //4 of a kind
+        y1.getDiceCollection().setValues(2, 2, 2, 2, 2);
+        assertEquals(10, y1.scoreFourOfAKind());
+        //full house
+        y1.getDiceCollection().setValues(6, 4, 6, 4, 6);
+        assertEquals(25, y1.scoreFullHouse());
+        //small straight
+        y1.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(0, y1.scoreSmallStraight());
+        //large straight
+        y1.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(0, y1.scoreLargeStraight());
+        //yahtzee
+        y1.getDiceCollection().setValues(2, 2, 2, 2, 2);
+        assertEquals(50, y1.scoreYahtzee());
+        //chance
+        y1.getDiceCollection().setValues(6, 1, 1, 1, 1);
+        assertEquals(10, y1.scoreChance());
+
+        //calculate bonus and verify bonus
+        assertEquals(0, y1.scoreBonus());
+
+        //verify that the available scoring types has one entry for YAHTZEE since it was scored
+        assertEquals(1, y1.getScoreCard().getAvailableScoreTypes().size());
+        assertTrue(y1.getScoreCard().getAvailableScoreTypes().contains(YahtzeeScoreTypes.YAHTZEE));
+
+        //verify final score
+        assertEquals(151, y1.getScoreCard().getScore());
+
+
+        //*** Game 2: 0 Bonus, 2 Yahtzee ***
+        Yahtzee y2 = new Yahtzee();
+
+        //Turn 1
+        y2.getDiceCollection().setValues(1, 1, 1, 1, 1);
+        assertEquals(50, y2.scoreYahtzee());
+        //Turn 2
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(6, y2.scoreTwos());
+        //Turn 3
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(3, y2.scoreThrees());
+        //Turn 4
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(0, y2.scoreFours());
+        //Turn 5
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 5);
+        assertEquals(5, y2.scoreFives());
+        //Turn 6
+        y2.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(30, y2.scoreSixes());
+        //3 of a kind
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(10, y2.scoreThreeOfAKind());
+        //4 of a kind
+        y2.getDiceCollection().setValues(2, 2, 2, 2, 2);
+        assertEquals(10, y2.scoreFourOfAKind());
+        //full house
+        y2.getDiceCollection().setValues(6, 4, 6, 4, 6);
+        assertEquals(25, y2.scoreFullHouse());
+        //small straight
+        y2.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(0, y2.scoreSmallStraight());
+        //large straight
+        y2.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(0, y2.scoreLargeStraight());
+        //yahtzee
+        y2.getDiceCollection().setValues(2, 2, 2, 2, 2);
+        assertEquals(100, y2.scoreYahtzee());
+        //chance
+        y2.getDiceCollection().setValues(6, 1, 1, 1, 1);
+        assertEquals(10, y2.scoreChance());
+
+        //calculate bonus and verify bonus
+        assertEquals(0, y2.scoreBonus());
+
+        //verify that the available scoring types has two entres...
+        // one for YAHTZEE and one for ONES
+        assertEquals(2, y2.getScoreCard().getAvailableScoreTypes().size());
+        assertTrue(y2.getScoreCard().getAvailableScoreTypes().contains(YahtzeeScoreTypes.ONES));
+        assertTrue(y2.getScoreCard().getAvailableScoreTypes().contains(YahtzeeScoreTypes.YAHTZEE));
+
+        //verify final score
+        assertEquals(249, y2.getScoreCard().getScore());
+
+        //*** Game 3: 1 Bonus, 0 Yahtzee ***
+        Yahtzee y3 = new Yahtzee();
+
+        //Turn 1
+        y3.getDiceCollection().setValues(1, 1, 1, 1, 1);
+        assertEquals(5, y3.scoreOnes());
+        //Turn 2
+        y3.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(6, y3.scoreTwos());
+        //Turn 3
+        y3.getDiceCollection().setValues(3, 3, 2, 2, 3);
+        assertEquals(9, y3.scoreThrees());
+        //Turn 4
+        y3.getDiceCollection().setValues(2, 4, 4, 4, 3);
+        assertEquals(12, y3.scoreFours());
+        //Turn 5
+        y3.getDiceCollection().setValues(2, 5, 2, 2, 5);
+        assertEquals(10, y3.scoreFives());
+        //Turn 6
+        y3.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(30, y3.scoreSixes());
+        //3 of a kind
+        y3.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(10, y3.scoreThreeOfAKind());
+        //4 of a kind
+        y3.getDiceCollection().setValues(2, 2, 2, 2, 2);
+        assertEquals(10, y3.scoreFourOfAKind());
+        //full house
+        y3.getDiceCollection().setValues(6, 4, 6, 4, 6);
+        assertEquals(25, y3.scoreFullHouse());
+        //small straight
+        y3.getDiceCollection().setValues(6, 6, 6, 6, 6);
+        assertEquals(0, y3.scoreSmallStraight());
+        //large straight
+        y3.getDiceCollection().setValues(2, 1, 2, 2, 3);
+        assertEquals(0, y3.scoreLargeStraight());
+        //yahtzee
+        y3.getDiceCollection().setValues(2, 1, 2, 2, 2);
+        assertEquals(0, y3.scoreYahtzee());
+        //chance
+        y3.getDiceCollection().setValues(6, 1, 1, 1, 1);
+        assertEquals(10, y3.scoreChance());
+
+        //calculate bonus and verify bonus
+        assertEquals(35, y3.scoreBonus());
+
+        //verify that no scoring types remain
+        assertEquals(0, y3.getScoreCard().getAvailableScoreTypes().size());
+
+        //verify final score
+        assertEquals(162, y3.getScoreCard().getScore());
+    }
+
+    @Test
     public void testAddToUpperScore() throws Exception
     {
         //ONES
@@ -280,6 +445,45 @@ public class YahtzeeScoreCardTest
     @Test
     public void testCalcScore() throws Exception
     {
+
+        Yahtzee y1 = new Yahtzee();
+        y1.getScoreCard().setUpperScore(99);
+        y1.getScoreCard().setLowerScore(33);
+
+        //verify that score is accurate
+        assertEquals(132, y1.getScoreCard().getScore());
+
+        //score the bonus
+        y1.scoreBonus();
+
+        //EXPECT BONUS
+        assertEquals(134, y1.getScoreCard().getUpperScore());
+        assertEquals(167, y1.getScoreCard().getScore());
+
+        //reset the upper and lower scores and verify change
+        y1.getScoreCard().setUpperScore(22);
+        y1.getScoreCard().setLowerScore(100);
+
+        //verify that score is accurate
+        assertEquals(122, y1.getScoreCard().getScore());
+        
+        //EXPECT 0 for BONUS
+        Yahtzee y2 = new Yahtzee();
+        y2.getScoreCard().setUpperScore(11);
+        y2.getScoreCard().setLowerScore(33);
+
+        //verify that score is accurate
+        assertEquals(44, y2.getScoreCard().getScore());
+
+        //score the bonus
+        y2.scoreBonus();
+
+        //verify that upperScore and score are accurate
+        assertEquals(11, y2.getScoreCard().getUpperScore());
+        assertEquals(44, y2.getScoreCard().getScore());
+
+
+
 
     }
 
