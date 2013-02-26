5a20bc035dade857202eb24e2d67ccbdbf43b378
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-25 19:24:09 -0800

    complete with numeric token comparator but need comments

--- src/edu/bellevuecollege/cs211/chapter13/NumericTokenComparator.java
+++ src/edu/bellevuecollege/cs211/chapter13/NumericTokenComparator.java
@@ -1,5 +1,5 @@
 package edu.bellevuecollege.cs211.chapter13;
-
+import java.util.Scanner;
 import java.util.Comparator;
 
 /**
@@ -10,7 +10,27 @@ public class NumericTokenComparator implements Comparator<String>
 {
     public int compare(String s1, String s2)
     {
-        return 0;
+        //find the number of tokens in s1
+        Scanner sc1 = new Scanner(s1);
+        int tokenCount1 = 0;
+
+        while(sc1.hasNext())
+        {
+            sc1.next();
+            tokenCount1++;
+        }
+
+        //find the number of tokens in s2
+        Scanner sc2 = new Scanner(s2);
+        int tokenCount2 = 0;
+
+        while(sc2.hasNext())
+        {
+            sc2.next();
+            tokenCount2++;
+        }
+
+        return tokenCount1 - tokenCount2;
     }
 
 
