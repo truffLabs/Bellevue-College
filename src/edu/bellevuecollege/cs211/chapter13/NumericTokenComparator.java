bf22fc08daf998c10303ec25ac498fbf7e6fb70e
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-25 17:34:22 -0800

    initial commit

---
+++ src/edu/bellevuecollege/cs211/chapter13/NumericTokenComparator.java
@@ -0,0 +1,17 @@
+package edu.bellevuecollege.cs211.chapter13;
+
+import java.util.Comparator;
+
+/**
+ * Created with IntelliJ IDEA. User: pakaehua Date: 2/25/13 Time: 5:29 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class NumericTokenComparator implements Comparator<String>
+{
+    public int compare(String s1, String s2)
+    {
+        return 0;
+    }
+
+
+}
