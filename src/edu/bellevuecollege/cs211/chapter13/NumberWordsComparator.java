c9f8f57b45cbcfd322a56f063466504826523858
Pat Kaehuaea <uncleakamai@gmail.com>
2013-02-26 21:11:39 -0800

    updated with comments

--- src/edu/bellevuecollege/cs211/chapter13/NumberWordsComparator.java
+++ src/edu/bellevuecollege/cs211/chapter13/NumberWordsComparator.java
@@ -1,17 +1,35 @@
 package edu.bellevuecollege.cs211.chapter13;
-
 import java.util.Comparator;
+import java.util.Scanner;
 
 /**
- * Created with IntelliJ IDEA. User: pakaehua Date: 2/25/13 Time: 5:29 PM To change this template use File | Settings |
- * File Templates.
+ * Compares two separate Strings according to the number of tokens they contain. This Comparator
+ * assumes that any non whitespace token is a word.
  */
 public class NumberWordsComparator implements Comparator<String>
 {
     public int compare(String s1, String s2)
     {
-        return 0;
+        //find the number of tokens in s1
+        Scanner sc1 = new Scanner(s1);
+        int tokenCount1 = 0;
+
+        while(sc1.hasNext())
+        {
+            sc1.next();
+            tokenCount1++;
+        }
+
+        //find the number of tokens in s2
+        Scanner sc2 = new Scanner(s2);
+        int tokenCount2 = 0;
+
+        while(sc2.hasNext())
+        {
+            sc2.next();
+            tokenCount2++;
+        }
+
+        return tokenCount1 - tokenCount2;
     }
-
-
 }
