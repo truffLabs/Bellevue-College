03da13059ed8609e473ac7e4bd7a1d0e8a1ee237
Pat Kaehuaea <uncleakamai@gmail.com>
2013-03-11 16:23:16 -1000

    everything but the last problem done

---
+++ src/edu/bellevuecollege/cs211/chapter17/IntTree.java
@@ -0,0 +1,290 @@
+package edu.bellevuecollege.cs211.chapter17;
+
+// Simple binary tree class that includes methods to construct a
+// tree of ints, to print the structure, and to print the data
+// using a preorder, inorder or postorder traversal.  The trees
+// built have nodes numbered starting with 1 and numbered
+// sequentially level by level with no gaps in the tree.  The
+// documentation refers to these as "sequential trees."
+
+
+import java.util.*;
+
+public class IntTree {
+
+    private IntTreeNode overallRoot;
+
+    public IntTree()
+    {
+        overallRoot = null;
+    }
+
+
+    public IntTree(IntTreeNode someRootNode)
+    {
+        this.overallRoot = someRootNode;
+    }
+
+    // pre : max > 0
+    // post: constructs a sequential tree with given number of
+    //       nodes
+    public IntTree(int max) {
+        if (max <= 0) {
+            throw new IllegalArgumentException("max: " + max);
+        }
+        overallRoot = buildTree(1, max);
+    }
+
+    // post: returns a sequential tree with n as its root unless
+    //       n is greater than max, in which case it returns an
+    //       empty tree
+    private IntTreeNode buildTree(int n, int max) {
+        if (n > max) {
+            return null;
+        } else {
+            return new IntTreeNode(n, buildTree(2 * n, max),
+                                   buildTree(2 * n + 1, max));
+        }
+    }
+
+    public int sum()
+    {
+
+        return sum(overallRoot);
+    }
+
+    private int sum(IntTreeNode someRoot)
+    {
+        //handle empty Binary Tree
+        if(someRoot == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return someRoot.data + sum(someRoot.left) + sum(someRoot.right);
+        }
+    }
+
+    public int countEmpty()
+    {
+        return countEmpty(overallRoot);
+    }
+
+    private int countEmpty(IntTreeNode someRoot)
+    {
+        //first base case
+        if(someRoot == null)
+        {
+            return 1;
+        }
+        //second base case...both null
+        else if (someRoot.left == null && someRoot.right == null)
+        {
+            return 2;
+        }
+        //third base case...left null
+        else if(someRoot.left == null)
+        {
+            return 1 + countEmpty(someRoot.right);
+        }
+        //last base case...right null
+        else if(someRoot.right == null)
+        {
+            return 1 + countEmpty(someRoot.left);
+        }
+        else
+        {
+            return countEmpty(someRoot.left) + countEmpty(someRoot.right);
+        }
+    }
+
+    public int countEvenBranches()
+    {
+        return countEvenBranches(overallRoot);
+    }
+
+    private int countEvenBranches(IntTreeNode someRoot)
+    {
+        //null base case
+        if(someRoot == null)
+        {
+            return 0;
+        }
+        //leaf base case
+        else if (someRoot.left == null && someRoot.right == null)
+        {
+            return 0;
+        }
+        //branch recursive case
+        else
+        {
+            //check if node is even
+            if(someRoot.data %2 == 0)
+            {
+                return 1 + countEvenBranches(someRoot.left) + countEvenBranches(someRoot.right);
+            }
+            //ignore, but recurse
+
+            return countEvenBranches(someRoot.left) + countEvenBranches(someRoot.right);
+        }
+    }
+
+    public void printLevel(int someNum)
+    {
+        if(someNum < 1)
+        {
+            throw new IllegalArgumentException();
+        }
+
+        int leftLevel = 1, rightLevel = 1;
+
+        printLevel(leftLevel, rightLevel, someNum, this.overallRoot);
+    }
+
+    private void printLevel(int leftLevel, int rightLevel, int targetLevel, IntTreeNode someRoot)
+    {
+        if(someRoot == null)
+        {
+            //do nothing
+        }
+        else if(leftLevel == targetLevel)
+        {
+            System.out.println(someRoot.data);
+        }
+        else if(rightLevel == targetLevel)
+        {
+            System.out.println(someRoot.data);
+        }
+        else
+        {
+            leftLevel++;
+            printLevel(leftLevel, rightLevel, targetLevel, someRoot.left);
+            rightLevel++;
+            printLevel(leftLevel, rightLevel, targetLevel, someRoot.right);
+        }
+    }
+
+    public void removeLeaves()
+    {
+        IntTreeNode prevRoot = overallRoot;
+        removeLeaves(prevRoot, overallRoot);
+    }
+
+    private void removeLeaves(IntTreeNode somePrevRoot, IntTreeNode someRoot)
+    {
+        //TODO: this doesn't work
+        //empty list
+        if(someRoot == null)
+        {
+            //do nothing
+        }
+        //leaves
+        else if(someRoot.left == null && someRoot.right == null)
+        {
+            somePrevRoot.left = null;
+            somePrevRoot.right = null;
+        }
+        else
+        {
+            somePrevRoot = someRoot;
+            removeLeaves(somePrevRoot, someRoot.left);
+            removeLeaves(somePrevRoot, someRoot.right);
+        }
+
+        /**
+        //empty list
+        if(someRoot == null)
+        {
+            //do nothing
+        }
+        //leaves
+        //TODO: Why does this generate a nullPointerException?
+        else if(someRoot.left.left == null && someRoot.right.right == null)
+        {
+            someRoot.left = null;
+            someRoot.right = null;
+        }
+        else
+        {
+            removeLeaves(someRoot.left);
+            removeLeaves(someRoot.right);
+        }
+        **/
+    }
+
+    // post: prints the tree contents using a preorder traversal
+    public void printPreorder() {
+        System.out.print("preorder:");
+        printPreorder(overallRoot);
+        System.out.println();
+    }
+
+    // post: prints the tree contents using a preorder traversal
+    // post: prints in preorder the tree with given root
+    private void printPreorder(IntTreeNode root) {
+        if (root != null) {
+            System.out.print(" " + root.data);
+            printPreorder(root.left);
+            printPreorder(root.right);
+        }
+    }
+
+    // post: prints the tree contents using a inorder traversal
+    public void printInorder() {
+        System.out.print("inorder:");
+        printInorder(overallRoot);
+        System.out.println();
+    }
+
+    // post: prints in inorder the tree with given root
+    private void printInorder(IntTreeNode root) {
+        if (root != null) {
+            printInorder(root.left);
+            System.out.print(" " + root.data);
+            printInorder(root.right);
+        }
+    }
+
+    // post: prints the tree contents using a postorder traversal
+    public void printPostorder() {
+        System.out.print("postorder:");
+        printPostorder(overallRoot);
+        System.out.println();
+    }
+
+    // post: prints in postorder the tree with given root
+    private void printPostorder(IntTreeNode root) {
+        if (root != null) {
+            printPostorder(root.left);
+            printPostorder(root.right);
+            System.out.print(" " + root.data);
+        }
+    }
+
+    // post: prints the tree contents, one per line, following an
+    //       inorder traversal and using indentation to indicate
+    //       node depth; prints right to left so that it looks
+    //       correct when the output is rotated.
+    public void printSideways() {
+        printSideways(overallRoot, 0);
+    }
+
+    // post: prints in reversed preorder the tree with given
+    //       root, indenting each line to the given level
+    private void printSideways(IntTreeNode root, int level) {
+        if (root != null) {
+            printSideways(root.right, level + 1);
+            for (int i = 0; i < level; i++) {
+                System.out.print("    ");
+            }
+            System.out.println(root.data);
+            printSideways(root.left, level + 1);
+        }
+    }
+
+    public IntTreeNode getOverallRoot()
+    {
+        return overallRoot;
+    }
+}
---
+++ src/edu/bellevuecollege/cs211/chapter17/IntTreeTest.java
@@ -0,0 +1,114 @@
+package edu.bellevuecollege.cs211.chapter17;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.sun.source.tree.BinaryTree;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA. User: pakaehua Date: 3/9/13 Time: 9:39 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class IntTreeTest
+{
+    IntTree ref1;
+    IntTree ref2;
+    IntTree ref3;
+    IntTree refNull;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        IntTreeNode ref1Root = new IntTreeNode(3, new IntTreeNode(5, new IntTreeNode(1, null, null), null),
+            new IntTreeNode(2, new IntTreeNode(4, null, null), new IntTreeNode(6, null, null)));
+
+        ref1 = new IntTree(ref1Root);
+
+        //ref1.printSideways();
+
+        IntTreeNode ref2Root = new IntTreeNode(2, new IntTreeNode(8, new IntTreeNode(0, null, null), null),
+                new IntTreeNode(1, new IntTreeNode(7, new IntTreeNode(4, null, null), null),
+                        new IntTreeNode(6, null, new IntTreeNode(9, null, null))));
+
+        ref2 = new IntTree(ref2Root);
+
+        //ref2.printSideways();
+
+        IntTreeNode ref3Root = new IntTreeNode(2, new IntTreeNode(3, new IntTreeNode(8, null, null),
+                new IntTreeNode(7, null, null)), new IntTreeNode(1, null, null));
+
+        ref3 = new IntTree(ref3Root);
+
+        //ref3.printSideways();
+
+        refNull = new IntTree();
+    }
+
+    public void testSum() throws Exception
+    {
+        assertEquals(21, ref1.sum());
+        assertEquals(37, ref2.sum());
+        assertEquals(21, ref3.sum());
+        assertEquals(0, refNull.sum());
+    }
+
+    @Test
+    public void testCountEmpty() throws Exception
+    {
+        assertEquals(7, ref1.countEmpty());
+        assertEquals(9, ref2.countEmpty());
+        assertEquals(6, ref3.countEmpty());
+        assertEquals(1, refNull.countEmpty());
+    }
+
+    @Test
+    public void testCountEvenBranches() throws Exception
+    {
+        assertEquals(1, ref1.countEvenBranches());
+        assertEquals(3, ref2.countEvenBranches());
+        assertEquals(1, ref3.countEvenBranches());
+        assertEquals(0, refNull.countEvenBranches());
+    }
+
+    @Test
+    public void testPrintLevel() throws Exception
+    {
+        //ref3.printLevel(3);
+    }
+
+    //test value less than 1
+    @Test (expected = IllegalArgumentException.class)
+    public void testPrintLevelBad() throws Exception
+    {
+        ref2.printLevel(0);
+    }
+
+    //test empty list
+    @Test
+    public void testPrintLevelBad2() throws Exception
+    {
+        //no exception should be thrown here
+        refNull.printLevel(1);
+    }
+
+    @Test
+    public void testRemoveLeaves() throws Exception
+    {
+        ref3.printSideways();
+        ref3.removeLeaves();
+
+        System.out.println("\n\n\n");
+
+        ref3.printSideways();
+
+        System.out.println("\n\n\n");
+
+    }
+
+
+}
