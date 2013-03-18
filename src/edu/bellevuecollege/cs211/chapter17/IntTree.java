a73ee0cf4e73ec8cfe063d85822489b244236a78
Pat Kaehuaea <uncleakamai@gmail.com>
2013-03-18 14:16:09 -0700

    binary search add method

--- src/edu/bellevuecollege/cs211/chapter17/IntTree.java
+++ src/edu/bellevuecollege/cs211/chapter17/IntTree.java
@@ -3,7 +3,7 @@ package edu.bellevuecollege.cs211.chapter17;
 // Simple binary tree class that includes methods to construct a
 // tree of ints, to print the structure, and to print the data
 // using a preorder, inorder or postorder traversal.  The trees
-// built have nodes numbered starting with 1 and numbered
+// built have nodes fnumbered starting with 1 and numbered
 // sequentially level by level with no gaps in the tree.  The
 // documentation refers to these as "sequential trees."
 
